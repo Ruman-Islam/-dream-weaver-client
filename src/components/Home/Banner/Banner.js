4e3c7ea1690f012de09e8a1f5650757b6c316aab
Ruman-Islam <rumanislam48@gmail.com>
2022-04-17 02:11:26 +0600

    banner component added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-
+        <div className='banner-container'>
+            <div className='banner-items-wrapper'>
+                <h2 className='banner-title'>Ready to take your beyond your imagination!</h2>
+                <p className='banner-text'>With the uniqueness of my concepts and hard working quality, I am always a step ahead from others.</p>
+                <p className='banner-text'>I take photos so perfectly that you will feel the same that you had felt in that very special day with yur very special one.</p>
+            </div>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
