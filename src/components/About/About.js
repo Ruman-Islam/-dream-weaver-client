8aa6113e2afa94979c7f18549bf937201b24f253
Ruman-Islam <rumanislam48@gmail.com>
2022-04-18 05:26:42 +0600

    about me section completed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Menubar from '../Shared/Menubar/Menubar';
+import profileImage from '../../assets/images/Ruman.jpg';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
+        <div className='about-container'>
             <Menubar />
-            <h1>This is about</h1>
+            <div className='container'>
+                <div className='about-wrapper'>
+                    <div className='about-description'>
+                        <h1>Ruman Islam</h1>
+                        <span>Goal</span>
+                        <p>A self-motivated, ambitious and enthusiastic web designer and developer with highly interested in JavaScript, HTML5, CSS3, and front-end web development with modern web tech like React.js. To work in Software industry with modern web technologies of different local and multinational Software/IT agency of Bangladesh and grow rapidly with increasing skills and responsibilities.</p>
+                        <p>My goal is to become a MERN Stack Web Developer. I'm passionate to make a clean, beautiful, professional, responsive Web site and maximum optimized way so that it performs well.</p>
+                    </div>
+                    <div className='about-photo'>
+                        <img src={profileImage} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Shared/Menubar/Menubar.js
+++ src/components/Shared/Menubar/Menubar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useFirebase from '../../../Firebase/useFirebase';
 import CustomLink from '../../CustomLink/CustomLink';
-// import defaultImg from '../../../assets/images/photoplaceholder.jpg';
+import defaultImg from '../../../assets/images/photoplaceholder.jpg';
 import './Menubar.css';
 
 const Menubar = () => {
@@ -12,28 +12,29 @@ const Menubar = () => {
         <nav className='container'>
             <div><h1 className='nav-title'>Dream Weaver</h1></div>
             <div className='navigation-items'>
+                <small className='username'>
+                    <strong>
+                        {user?.displayName ? `Hi, ${user?.displayName.split(' ')[0]}!` : ''}
+                    </strong>
+                </small>
                 <CustomLink to='/home'>Home</CustomLink>
                 <CustomLink to="/Packages">Packages</CustomLink>
                 <CustomLink to="/about">About me</CustomLink>
                 <CustomLink to="/blog">Blog</CustomLink>
-                {/* <div>
-                    <img style={{ width: '30px', height: '30px', borderRadius: '50%' }} src={user?.photoURL ? user?.photoURL : defaultImg} alt="" />
-                </div> */}
+
                 {user?.email ?
                     <div>
-                        <button style={{ marginTop: '15px' }} onClick={handleSignOut}>
+                        <button onClick={handleSignOut}>
                             Log out
                         </button>
-                        <small style={{ marginLeft: '12px', fontSize: '12px', display: 'block' }}>
-                            <strong>
-                                {user.displayName ? `Hi, ${user.displayName.split(' ')[0]}` : ''}
-                            </strong>
-                        </small>
                     </div>
                     :
                     <button onClick={() => navigate('/login')}>
                         Log In
                     </button>}
+                <div className='user-profile-photo'>
+                    <img src={user?.photoURL ? user?.photoURL : defaultImg} alt="" />
+                </div>
             </div>
         </nav>
     );
